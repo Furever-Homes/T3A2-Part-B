b001f39a02e51301f48f8e93e12b20e05ee63d27
hendricwidjaja <hcwidjaja@gmail.com>
2025-02-23 22:31:10 +1100

    feat: Create pseudocode for petController.js

---
+++ src/server/src/controllers/petController.js
@@ -0,0 +1,19 @@
+const { Pet } = require("../models/PetModel");
+
+// Get all pets
+
+// Get a specific pet
+
+// Create a pet
+
+// Update existing pet details
+
+// Remove a pet
+
+// module.exports = {
+//     getAllPets,
+//     getPet,
+//     createPet,
+//     updatePet,
+//     deletePet
+// }
