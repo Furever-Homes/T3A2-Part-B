ba8c1b9cb9bbee763c66ef0dc399165a6aeb3317
jayel888 <15644@coderacademy.edu.au>
2025-02-24 21:24:20 +1100

    feat: Created UserModel, input validiation for UserSchema

---
+++ src/server/src/models/UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"]
+    }
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+
+module.exports = {
+    UserModel
+}
