f15e5744916f511ea2f80ecb844048d336d0704c
Lissy Duncan <duncanlissy@gmail.com>
2025-02-28 09:59:08 +1100

    Amended Navbar component

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,42 +1,20 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-import Navbar from "../components/Navbar";
-import Home from "./pages/Home";
-import Explore from "./pages/Explore";
-import Profile from "./pages/Profile";
-import Adopt from "./pages/Adopt";
-import Favourites from "./pages/Favourites";
-import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home.jsx";
+import Explore from "./pages/Explore.jsx";
+import Profile from "./pages/Profile.jsx";
+import Adopt from "./pages/Adopt.jsx";
+import Favourites from "./pages/Favourites.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* make the Navbar appear on all pages */}
+      <Navbar />
 
       {/* Routes for the pages */}
       <Routes>
@@ -45,7 +23,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/adopt" element={<Adopt />} />
         <Route path="/favourites" element={<Favourites />} />
-        <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
+        <Route path="*" element={<NotFound />} /> {/* for 404 */}
       </Routes>
     </Router>
   );
