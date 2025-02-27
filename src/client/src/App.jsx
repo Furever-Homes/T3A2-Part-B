0632d2977f870fbf9bf4c769229ab8450d93c853
Lissy Duncan <duncanlissy@gmail.com>
2025-02-28 09:48:42 +1100

    Added React Router setup

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,1 +1,54 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+import Navbar from "../components/Navbar";
+import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+import Profile from "./pages/Profile";
+import Adopt from "./pages/Adopt";
+import Favourites from "./pages/Favourites";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Router>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      {/* Routes for the pages */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/adopt" element={<Adopt />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
