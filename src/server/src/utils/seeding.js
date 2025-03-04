e1f802fb2b33638e822a7b9d5b20340c9ae1800f
hendricwidjaja <hcwidjaja@gmail.com>
2025-03-04 22:09:48 +1100

    fix: Resolve bcrypt import typo

--- src/server/src/utils/seeding.js
+++ src/server/src/utils/seeding.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 const { dbConnect, dbDisconnect } = require("./database");
 const { User } = require("../models/UserModel");
