15cb644f48814cb224fe1347d15233c7b68cf307
hendricwidjaja <hcwidjaja@gmail.com>
2025-03-08 11:07:00 +1100

    feat: Update petModel.js for default image of pet based on animalType attribute

--- src/server/src/models/PetModel.js
+++ src/server/src/models/PetModel.js
@@ -9,7 +9,7 @@ const PetSchema = new mongoose.Schema(
     animalType: {
       type: String,
       enum: ["Cat", "Dog", "Other"],
-      required: true
+      required: true,
     },
     age: {
       type: Number,
@@ -34,14 +34,23 @@ const PetSchema = new mongoose.Schema(
       required: true,
     },
     image: {
-      type: String
+      type: String,
+      default: function () {
+        if (!this.image) {
+          return {
+            Dog: process.env.CLOUDINARY_DEFAULT_DOG,
+            Cat: process.env.CLOUDINARY_DEFAULT_CAT,
+            Other: process.env.CLOUDINARY_DEFAULT_OTHER,
+          }[this.animalType] || process.env.CLOUDINARY_DEFAULT_OTHER;
+        }
+        return this.image;
+      },
     },
   },
   { timestamps: true }
 );
 
 const Pet = mongoose.model("Pet", PetSchema);
-
 module.exports = {
-  Pet,
+    Pet
 };
